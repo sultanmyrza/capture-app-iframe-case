1f725e77ce5b36fa907f0f11c0d15ef953197c5c
sultanmyrza <sultanmyrza@gmail.com>
2022-08-18 13:01:51 +0600

    finish capture tab

--- src/app/features/home/capture-tab/capture-tab.component.ts
+++ src/app/features/home/capture-tab/capture-tab.component.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/member-ordering */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { DiaBackendAsset, PaginatedResponse } from './capture-tab.data';
 
 @Component({
   selector: 'app-capture-tab',
@@ -6,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./capture-tab.component.scss'],
 })
 export class CaptureTabComponent implements OnInit {
+  private readonly url = 'https://api.numbersprotocol.io/api/v3/assets/';
 
-  constructor() { }
+  private readonly headers = {
+    'Content-Type': 'application/json',
+    // temporary user token for ionic team
+    Authorization: `token ae62c562b5816f4ed35be68e11e4b3c83c96bd58`,
+  };
+
+  captures$ = this.http
+    .get<PaginatedResponse<DiaBackendAsset>>(this.url, {
+      headers: this.headers,
+    })
+    .pipe(map((response) => response.results));
+
+  constructor(readonly http: HttpClient) {}
 
   ngOnInit() {}
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 
-const imports = [MaterialModule];
+const imports = [HttpClientModule, MaterialModule];
 
 @NgModule({
   declarations: [],
